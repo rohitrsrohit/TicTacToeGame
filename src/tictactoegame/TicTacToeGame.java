852faa1d941fc372bbc5f9b9589c0838b111f073
Rohit Patro <rohitpatro6@gmail.com>
2021-07-21 22:14:21 +0530

    Added:UC1 Board Initialized

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -1,8 +1,22 @@
 package tictactoegame;
 
 public class TicTacToeGame {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Tic Tac Toe Game");
+	/*
+	 * creating method with size 10
+	 */
+	public static char[] createBoard()
+	{
+		//creating array with size 10 
+		char[] board=new char[10];
+		for(int i=1;i<board.length;i++) {
+			board[i]=' ';
+		}
+		return board;
 	}
-
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tac Toe Game...");
+		//calling createBoard method in main method		 
+		char[] board = createBoard();
+	}
 }
